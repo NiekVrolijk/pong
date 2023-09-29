b0efe4ff3cd6dc1240158c9cb365341a008ead67
NiekVrolijk <144125005+NiekVrolijk@users.noreply.github.com>
2023-09-29 12:14:48 +0200

    Initial commit

---
+++ Assets/scripts/pong.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pong : MonoBehaviour
+{
+
+    public float xPosition = 2f;
+    public float yPosition = 2f;
+    public float xdirection = 1f;
+    public float ydirection = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(xPosition, yPosition, 0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.position = new Vector3(xPosition, yPosition, 0f);
+
+        if (xdirection == 1f)
+        {
+            xPosition = xPosition + 0.02f;
+        }
+        if (xdirection == 0f)
+        {
+            xPosition = xPosition + -0.02f;
+        }
+
+        if (xPosition > 9.8f)
+        {
+            xdirection = 0f;
+        }
+        if (xPosition < -9.8f)
+        {
+            xdirection = 1f;
+        }
+
+
+        if (ydirection == 1f)
+        {
+            yPosition = yPosition + 0.02f;
+        }
+        if (ydirection == 0f)
+        {
+            yPosition = yPosition + -0.02f;
+        }
+
+        if (yPosition > 4.8f)
+        {
+            ydirection = 0f;
+        }
+        if (yPosition < -4.8f)
+        {
+            ydirection = 1f;
+        }
+    }
+}
